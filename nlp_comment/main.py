4ee62d90993603ead4c2079f127f40580693c742
François Sémécurbe <francoissemecurbe@gmail.com>
2023-08-29 11:00:22 +0200

    Update main.py

--- nlp_comment/main.py
+++ nlp_comment/main.py
@@ -1,5 +1,23 @@
-import pandas as pd
-import numpy as np
-import spacy
+import micropip
+from pyodide import to_js
 
-print('toto')
+PACKAGES_PATH = "/static"
+CUSTOM_BUILT_PKG_NAMES = list(map(lambda name: f"{PACKAGES_PATH}/{name}-cp310-cp310-emscripten_3_1_14_wasm32.whl", [
+    "blis-0.7.8",
+    "cymem-2.0.6",
+    "murmurhash-1.0.7",
+    "preshed-3.0.6",
+    "srsly-2.4.3",
+    "thinc-8.1.0",
+    "spacy-3.4.0",
+]))
+SPACY_MODEL_NAME = "en_core_web_sm"
+SPACY_MODEL_VERSION = "3.4.0"
+
+await micropip.install([
+        f"{PACKAGES_PATH}/{SPACY_MODEL_NAME}-{SPACY_MODEL_VERSION}-py3-none-any.whl"
+    ] + CUSTOM_BUILT_PKG_NAMES)  # type: ignore
+
+import pandas
+import matplotlib
+import spacy
